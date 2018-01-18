936df3d48af9df278ddd5d77aebb5101b387b15b
Your Name <you@example.com>
2018-01-18 22:02:08 +0000

    pos of cs output

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <libsnark/relations/constraint_satisfaction_problems/r1cs/examples/r1cs_examples.hpp>
 #include <libsnark/zk_proof_systems/ppzksnark/r1cs_gg_ppzksnark/r1cs_gg_ppzksnark.hpp>
 
+
+
 using namespace libsnark;
 
 /**
@@ -22,8 +24,8 @@ bool run_r1cs_gg_ppzksnark(const r1cs_example<libff::Fr<ppT> > &example)
 {
     libff::print_header("R1CS GG-ppzkSNARK Generator");
     r1cs_gg_ppzksnark_keypair<ppT> keypair = r1cs_gg_ppzksnark_generator<ppT>(example.constraint_system);
-    printf("\n"); libff::print_indent(); libff::print_mem("after generator");
 
+    
     libff::print_header("Preprocess verification key");
     r1cs_gg_ppzksnark_processed_verification_key<ppT> pvk = r1cs_gg_ppzksnark_verifier_process_vk<ppT>(keypair.vk);
 
@@ -43,17 +45,66 @@ bool run_r1cs_gg_ppzksnark(const r1cs_example<libff::Fr<ppT> > &example)
     return ans;
 }
 
+template<typename FieldT>
+void r1cs_to_json(r1cs_constraint_system<FieldT> constraints)
+{
+    std::cout << " a = [ ";
+    for (size_t c = 0; c < constraints.constraints.size(); ++c)
+    {
+        for (const linear_term<FieldT>& lt : constraints.constraints[c].a.terms)
+        {  
+            std::cout << "\n[";
+            for (uint i=0;i<constraints.constraints.size();++i) 
+            {
+                if(lt.index == i) {
+                    std::cout /*<< "    "<< c << ", " << lt.index << "=>"*/ << lt.coeff << ",";
+                }
+                else {
+                    std::cout /*<< "    "<< c << ", " << i << "=>"*/ << "0" << ",";
+                }
+            }
+            std::cout << "]\n";
+        }
+    }
+    std::cout << "]\n";
+
+    std::cout << " b = [ ";
+    for (size_t c = 0; c < constraints.constraints.size(); ++c)
+    {   
+        for (const linear_term<FieldT>& lt : constraints.constraints[c].b.terms)
+        {   
+            std::cout << c << ", " << lt.index << "=>" << lt.coeff << ",\n";
+        }
+    }
+    std::cout << "]";
+
+    std::cout << " c = [ ";
+    for (size_t c = 0; c < constraints.constraints.size(); ++c)
+    {   
+        for (const linear_term<FieldT>& lt : constraints.constraints[c].c.terms)
+        {   
+            std::cout << c << ", " <<  lt.index << "=>" << lt.coeff << ",\n";
+        }
+    }
+    std::cout << "]";
+}
+
+
 template<typename ppT>
 void test_r1cs_gg_ppzksnark(size_t num_constraints, size_t input_size)
 {
     r1cs_example<libff::Fr<ppT> > example = generate_r1cs_example_with_binary_input<libff::Fr<ppT> >(num_constraints, input_size);
-    const bool bit = run_r1cs_gg_ppzksnark<ppT>(example);
-    assert(bit);
+
+//    std::cout << num_constraints;
+//    std::cout << input_size;
+    r1cs_to_json(example.constraint_system);
+
+//    const bool bit = run_r1cs_gg_ppzksnark<ppT>(example);
+//    assert(bit);
 }
 
 int main () {
     default_r1cs_gg_ppzksnark_pp::init_public_params();
-    test_r1cs_gg_ppzksnark<default_r1cs_gg_ppzksnark_pp>(1000, 100);
-
+    test_r1cs_gg_ppzksnark<default_r1cs_gg_ppzksnark_pp>(5, 1);
     return 0;
 }
