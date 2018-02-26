e4558dca22513aafa8bea65f552f7fab6f57715f
Your Name <you@example.com>
2018-02-26 13:46:53 +0000

    removed extra circuits

--- src/main.cpp
+++ src/main.cpp
@@ -280,39 +280,10 @@ void test_r1cs_ppzksnark(size_t num_constraints)
     
 }
 
-
-template<typename FieldT>
-void hash_r1cs_gg_ppzksnark(size_t num_constraints)
-{
-    protoboard<libff::Fr<FieldT>> pb;
-
-    digest_variable<libff::Fr<FieldT>> left(pb, SHA256_digest_size, "left");
-    digest_variable<libff::Fr<FieldT>> right(pb, SHA256_digest_size, "right");
-    digest_variable<libff::Fr<FieldT>> output(pb, SHA256_digest_size, "output");
-
-    sha256_two_to_one_hash_gadget<libff::Fr<FieldT>> f(pb, left, right, output, "f");
-    f.generate_r1cs_constraints();
-    printf("Number of constraints for sha256_two_to_one_hash_gadget: %zu\n", pb.num_constraints());
-
-    const libff::bit_vector left_bv = libff::int_list_to_bits({0x426bc2d8, 0x4dc86782, 0x81e8957a, 0x409ec148, 0xe6cffbe8, 0xafe6ba4f, 0x9c6f1978, 0xdd7af7e9}, 32);
-    const libff::bit_vector right_bv = libff::int_list_to_bits({0x038cce42, 0xabd366b8, 0x3ede7e00, 0x9130de53, 0x72cdf73d, 0xee825114, 0x8cb48d1b, 0x9af68ad0}, 32);
-    const libff::bit_vector hash_bv = libff::int_list_to_bits({0xeffd0b7f, 0x1ccba116, 0x2ee816f7, 0x31c62b48, 0x59305141, 0x990e5c0a, 0xce40d33d, 0x0b1167d1}, 32);
-
-    left.generate_r1cs_witness(left_bv);
-    right.generate_r1cs_witness(right_bv);
-
-    f.generate_r1cs_witness();
-    output.generate_r1cs_witness(hash_bv);
-    // left = 256 , right = 256, output = 256 , 256 * 3 = 768 
-    r1cs_to_json(pb.get_constraint_system(), 256*3, "r1cs.json" ); 
-    dump_key(pb, 256*3);
-    assert(pb.is_satisfied()); 
-}
-
 int main () {
 
     libff::alt_bn128_pp::init_public_params();
     test_r1cs_ppzksnark<alt_bn128_pp>(4);
-//    hash_r1cs_gg_ppzksnark<alt_bn128_pp>(4);
+
     return 0;
 }
